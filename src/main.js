c89e2865d6ec3dc793c251c5f937ccae810ae442
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-04-17 16:08:32 +0200

    add import scss

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import { router } from "./router";
-import * as bootstrap from "bootstrap";
+import { createApp } from 'vue';
+
+//import style file
+import './scss/general.scss';
+import * as bootstrap from 'bootstrap';
+
+import App from './App.vue';
+import {router} from './router';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+
+
+
+import {faXTwitter,faYoutube,faGithub, faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons'
+library.add(far,fas,fab,faXTwitter,faYoutube,faGithub,faFacebook,faInstagram)
 
 const app = createApp(App);
 app.use(router);
-app.mount("#app");
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
