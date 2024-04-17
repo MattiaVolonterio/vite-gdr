4a9171767f5f6f5ced31220ffe12e47380b78aa7
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-04-17 15:55:50 +0200

    fix router

--- src/router.js
+++ src/router.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AppHome from "./pages/AppHome.vue";
-import ProjectsList from "./pages/ProjectsList.vue";
-import ProjectDetail from "./pages/ProjectDetail.vue";
+
 
 const router = createRouter({
   history: createWebHistory(),
